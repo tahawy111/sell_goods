1f9d55163327941b4ce12f1c0653e3707e6fea30
tahawy7 <amer.zp582@gmail.com>
2022-05-26 17:07:35 +0200

    .

---
+++ models/productModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: String,
+    // price: Number,
+    image: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("product", productSchema);
