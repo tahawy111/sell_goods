c256ef08270b690c6a648d639858aad1f23b690f
tahawy7 <amer.zp582@gmail.com>
2022-05-31 17:42:11 +0200

    .

--- config/passport.js
+++ config/passport.js
@@ -11,18 +11,46 @@ module.exports = (passport) => {
       (username, password, done) => {
         // Match Admin
         Admin.findOne({ username: username })
-          .then((user) => {
-            if (!user) {
+          .then((admin) => {
+            if (!admin) {
               return done(null, false, {
                 message: "That username is not registred! ",
               });
             }
 
             // Match Password
-            bcrypt.compare()
+            bcrypt.compare(password, admin.password, (err, isMatch) => {
+              if (err) throw err;
+
+              if (isMatch) {
+                return done(null, user);
+              } else {
+                return done(null, false, { message: "Password incorrect" });
+              }
+            });
+            // .then((isMatch) => {
+            //   if (isMatch) {
+            //     return done(null, user);
+            //   } else {
+            //     return done(null, false, { message: "Password incorrect" });
+            //   }
+            // })
+            // .catch((err) => {
+            //   throw err;
+            // });
           })
           .catch((err) => console.log(err));
       }
     )
   );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
 };
