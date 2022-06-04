2b6b1373532b69cbaa00793db9dd7b78d7296cd1
tahawy7 <amer.zp582@gmail.com>
2022-06-04 22:58:20 +0200

    Finish cart

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -168,23 +168,29 @@ router.get("/cart/decProduct/:index", ensureAuthenticated, (req, res) => {
 
 router.get("/cart/deleteProduct/:index", ensureAuthenticated, (req, res) => {
   const { index } = req.params;
-
   const productsArray = req.user.cart.selectedProduct;
 
-  req.user.cart.totalQuantity =
-    req.user.cart.totalQuantity - req.user.cart.selectedProduct[index].quantity;
+  if (req.user.cart.selectedProduct.length <= 1) {
+    CartModel.findByIdAndDelete(req.user.cart._id).then((result) => {
+      res.redirect("/");
+    });
+  } else {
+    req.user.cart.totalQuantity =
+      req.user.cart.totalQuantity -
+      req.user.cart.selectedProduct[index].quantity;
 
-  req.user.cart.totalPrice =
-    req.user.cart.totalPrice - req.user.cart.selectedProduct[index].price;
+    req.user.cart.totalPrice =
+      req.user.cart.totalPrice - req.user.cart.selectedProduct[index].price;
 
-  productsArray.splice(index, 1);
+    productsArray.splice(index, 1);
 
-  CartModel.updateOne({ _id: req.user.cart._id }, { $set: req.user.cart })
-    .then((doc) => {
-      console.log(doc);
-      res.redirect("/cart");
-    })
-    .catch((err) => console.log(err));
+    CartModel.updateOne({ _id: req.user.cart._id }, { $set: req.user.cart })
+      .then((doc) => {
+        console.log(doc);
+        res.redirect("/cart");
+      })
+      .catch((err) => console.log(err));
+  }
 
   console.log(req.user.cart);
 });
