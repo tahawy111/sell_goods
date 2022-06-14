a43f272329c6104fc56087491a1638b95685cb4d
tahawy7 <amer.zp582@gmail.com>
2022-06-14 23:22:27 +0200

    fix datatables search

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 
 require("./routes")(app);
 
-// mongodb+srv://admin:admin123456@cluster0.04rgz.mongodb.net/images?retryWrites=true&w=majority
+//  mongodb+srv://admin:admin123456@cluster0.04rgz.mongodb.net/images?retryWrites=true&w=majority
 // mongodb://localhost:27017/sell_goods
 mongoose
   .connect(
