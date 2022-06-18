4100aef06d4b462a34344f6f5811ce1dfc0941d1
tahawy7 <amer.zp582@gmail.com>
2022-06-18 21:59:40 +0200

    .

--- models/ComplaintModel.js
+++ models/ComplaintModel.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const complaintSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     complaintContent: { type: String, required: true },
-    number: { type: Number, required: true },
+    number: { type: String, required: true },
     solved: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
 );
-module.exports = mongoose.model("complaint", complaintSchema);
+module.exports = mongoose.model('complaint', complaintSchema);
