7d076ce90a7928704149dfd9abd8e7cce73311d6
tahawy111 <amer.zp582@gmail.com>
2022-06-22 16:01:21 +0200

    .

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ require('./routes')(app);
 // mongodb://localhost:27017/sell_goods
 mongoose
   .connect(
-    'mongodb+srv://admin:admin123456@cluster0.04rgz.mongodb.net/images?retryWrites=true&w=majority'
+    'mongodb://localhost:27017/sell_goods'
   )
   .then(() => {
     console.log('DB Connected');
